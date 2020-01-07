231a7362ba0b03ef71323d307716bb7d08f0fd29
Stugeh <38357751+Stugeh@users.noreply.github.com>
2020-01-08 00:32:20 +0200

    Add files via upload

---
+++ C(first language, dont judge)/centauri/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ int etäisyys
+ int aika
+ double et_valvuosi
+ double valovuosi
+ double aika
+    return 0;
+}
---
+++ C(first language, dont judge)/suunnitelma/ottosuunnitelma.c
@@ -0,0 +1,15 @@
+if char == a do ""
+ else
+    if char == b do ""
+      else
+        if char == c do ""
+          else
+            if char == d do ""
+              else
+                if char == e do ""
+                  else
+                    if char == f do ""
+                      else
+                        if char == g do ""
+                          else
+                            if char == h do ""
